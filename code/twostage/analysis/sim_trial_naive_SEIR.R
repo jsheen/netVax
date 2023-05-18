5c31b3577d87b4dfe3114131ce98582043d641c8
jsheen <jsheen@sas.upenn.edu>
2023-05-18 15:33:34 -0400

    new files

--- code/twostage/analysis/sim_trial_naive_SEIR.R
+++ code/twostage/analysis/sim_trial_naive_SEIR.R
@@ -11,11 +11,11 @@ N_trials = 1000 # Number of trial simulations to conduct
 n_perm = 1000
 cutoff = 120
 alpha = 0.05
-num_bootstrap_sample = 1
-assignment_mechanisms = c(0, 0)
-N_assignment_mechanism_sets = 30
+num_bootstrap_sample = 1000
+assignment_mechanisms = c(0, 0.1)
+N_assignment_mechanism_sets = 5
 N_groups = length(assignment_mechanisms) * N_assignment_mechanism_sets
-R0_vax = 0
+R0_vax = 1.1
 if (N_groups %% length(assignment_mechanisms) != 0) {
   stop('The number of groups should be divisible by the number of assignment mechanisms.')
 }
